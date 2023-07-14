1e5ad192421a8f8094c8ac4c1849b94313afa6d2
rishi.shendkar <rishikesh.shendkar@gmail.com>
2023-07-15 00:24:41 +0530

    settings

--- ObservableConfigEditor/settings.py
+++ ObservableConfigEditor/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "ObservableConfigEditor.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'editor',
+        'USER': 'root',
+        'PASSWORD': 'rishi123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
